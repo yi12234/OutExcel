63b23c65d2a2c42f8df7d10cfaac409f174c7e4f
yi12234 <18865541037@163.com>
2018-01-02 15:39:34 +0800

    移动权限

--- myexcel/src/main/java/com/example/myexcel/BaseExcel.java
+++ myexcel/src/main/java/com/example/myexcel/BaseExcel.java
@@ -1,6 +1,9 @@
 package com.example.myexcel;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 
 import com.example.myexcel.ExcelUtils;
 
@@ -17,10 +20,33 @@ import static com.example.myexcel.Tools.makeDir;
  * @explain :
  */
 public abstract class BaseExcel {
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE };
 
     public static void BaseExcel(String[]title, String filepath, String fileName, String sheetName, ArrayList<ArrayList<String>> data, Activity activity) {
-       File file = new File(filepath);
-       makeDir(file);
+
+        /**
+         8      * Checks if the app has permission to write to device storage
+         9      *
+         10      * If the app does not has permission then the user will be prompted to
+         11      * grant permissions
+         12      *
+         13      * @param activity
+         14      */
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE);
+        }
+        File file = new File(filepath);
+        makeDir(file);
         ExcelUtils.initExcel(fileName,sheetName, title);
         ExcelUtils.writeObjListToExcel(data, fileName, activity);
     }
